f9286c3a6339235054316a54f0a502e27102be66
capatterson1970 <capatterson70@yahoo.com>
2020-08-08 20:43:32 -0500

    Need to add tests but everything is working

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// const assert = require('assert');
+ const assert = require('assert');
 // const readline = require('readline');
 // const rl = readline.createInterface({
 //   input: process.stdin,
